1eb416c2a647f3030e5ac61f60c54af4dadee9f9
Victor Hugo <vhpavoni16@gmail.com>
2021-10-21 21:19:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'vhpavoni',
+  youtube: 'victorpavoni',
+  facebook: 'vhpavoni',
+  instagram: 'victorpavoni',
+  twitter: 'VhPavoni'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
